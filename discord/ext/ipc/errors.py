e260ad78c54b7098568048896ee31b069b51d356
MiroslavRosenov <57327793+MiroslavRosenov@users.noreply.github.com>
2022-06-02 20:27:33 +0300

    Update errors.py

--- discord/ext/ipc/errors.py
+++ discord/ext/ipc/errors.py
@@ -1,31 +1,26 @@
-from discord import DiscordException
-
+from discord import ClientException
+from typing import Tuple
 
 class IPCError(DiscordException):
     """Base IPC exception class"""
-
-    pass
+    __slots__: Tuple[str, ...] = ()
 
 
 class NoEndpointFoundError(IPCError):
     """Raised upon requesting an invalid endpoint"""
-
     pass
 
 
 class ServerConnectionRefusedError(IPCError):
     """Raised upon a server refusing to connect / not being found"""
-
     pass
 
 
 class JSONEncodeError(IPCError):
     """Raise upon un-serializable objects are given to the IPC"""
-
     pass
 
 
 class NotConnected(IPCError):
     """Raised upon websocket not connected"""
-
     pass
