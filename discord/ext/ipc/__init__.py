958d7283b1ac4ac50a813532c33ce309a6f0380b
MiroslavRosenov <57327793+MiroslavRosenov@users.noreply.github.com>
2022-06-02 20:11:18 +0300

    Update __init__.py

--- discord/ext/ipc/__init__.py
+++ discord/ext/ipc/__init__.py
@@ -7,5 +7,5 @@ from discord.ext.ipc.errors import *
 
 _VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
 
-version = "2.1.1"
+version = "2.1.2"
 version_info = _VersionInfo(2, 1, 1, "final", 0)
