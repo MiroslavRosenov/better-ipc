71e6dab80d134003bf28e75bd5b3fbfe789860f4
MiroslavRosenov <miroslav.rosenov39@gmail.com>
2022-06-04 19:09:48 +0300

    setup.py rewrite

--- discord/ext/ipc/__init__.py
+++ discord/ext/ipc/__init__.py
@@ -1,11 +1,25 @@
-import collections
+"""
+Better IPC
+~~~~~~
+A high-performance inter-process communication 
+library designed to work with the latest version of discord.py
+:license: Apache License 2.0
+"""
+
+try:
+    import discord
+except ImportError:
+    raise RuntimeError("You must have discord.py installed!")
+else:
+    if not discord.__version__.startswith("2.0"):
+        raise RuntimeError(
+            "You must have discord.py 2.0 installed in order for Better IPC to work!"
+        )
+
+__version__ = "1.0.2"
+__title__ = "better-ipc"
+__author__ = "DaPandaOfficial"
 
 from discord.ext.ipc.client import Client
 from discord.ext.ipc.server import Server
 from discord.ext.ipc.errors import *
-
-
-_VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
-
-version = "1.0.1"
-version_info = _VersionInfo(1, 0, 0, "final", 0)
--- setup.py
+++ setup.py
@@ -1,52 +1,15 @@
-import re
 import setuptools
 from pathlib import Path
-
-classifiers = [
-    "Development Status :: 5 - Production/Stable",
-    "Framework :: AsyncIO",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: Apache Software License",
-    "Natural Language :: English",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Topic :: Communications",
-    "Topic :: Internet",
-    "Topic :: Software Development",
-    "Topic :: Software Development :: Libraries",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-]
+from discord.ext.ipc.__init__ import __version__, __title__
 
 with open("requirements.txt") as stream:
     raw = stream.read().splitlines()
-
     requirements = [x for x in raw if not x.startswith("git+")]
-    dependencies = [x for x in raw if x.startswith("git+")]
-
-packages = [
-    "discord.ext.ipc",
-]
-
-project_urls = {
-    "Source": "https://github.com/MiroslavRosenov/better-ipc",
-    "Issue Tracker": "https://github.com/MiroslavRosenov/better-ipc/issues",
-}
-
-_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
-
-with open("discord/ext/ipc/__init__.py") as stream:
-    match = re.search(_version_regex, stream.read(), re.MULTILINE)
-
-version = match.group(2)
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
-    name="better-ipc",
     author="DaPandaOfficial",
     python_requires=">=3.8.0",
     license="Apache Software License",
@@ -55,11 +18,31 @@ setuptools.setup(
     url="https://github.com/MiroslavRosenov/better-ipc",
     download_url="https://github.com/MiroslavRosenov/better-ipc/archive/refs/tags/1.0.tar.gz",
     description="A high-performance inter-process communication library designed to work with the latest version of discord.py",
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Source": "https://github.com/MiroslavRosenov/better-ipc",
+        "Issue Tracker": "https://github.com/MiroslavRosenov/better-ipc/issues",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=["better_ipc", "ipc", "python", "discord.py"],
     long_description=long_description,
     install_requires=requirements,
-    dependency_links=dependencies,
-    packages=packages,
-    project_urls=project_urls,
-    classifiers=classifiers,
-    version=version,
+    name=__title__,
+    version=__version__,
 )
