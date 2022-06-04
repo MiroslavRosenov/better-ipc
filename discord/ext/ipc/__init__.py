f2c6a9e6d6373634c4f44ba16737b2d9a186b24f
MiroslavRosenov <miroslav.rosenov39@gmail.com>
2022-06-04 18:44:18 +0300

    Version bump

--- discord/ext/ipc/__init__.py
+++ discord/ext/ipc/__init__.py
@@ -7,5 +7,5 @@ from discord.ext.ipc.errors import *
 
 _VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
 
-version = "1.0.0"
+version = "1.0.1"
 version_info = _VersionInfo(1, 0, 0, "final", 0)
