714e5de06817d1ac1e2147d2ce70cffa2a9b0f23
MiroslavRosenov <miroslav.rosenov39@gmail.com>
2022-06-04 19:27:05 +0300

    Added discord.py forks fix

--- discord/ext/ipc/__init__.py
+++ discord/ext/ipc/__init__.py
@@ -6,6 +6,17 @@ library designed to work with the latest version of discord.py
 :license: Apache License 2.0
 """
 
+try:
+    import discord
+except ImportError:
+    raise RuntimeError("You must have discord.py installed!")
+else:
+    if not discord.__version__.startswith("2.0"):
+        raise RuntimeError(
+            "You must have discord.py 2.0 installed in order for Better IPC to work!"
+        )
+
+
 __version__ = "1.0.2"
 __title__ = "better-ipc"
 __author__ = "DaPandaOfficial"
