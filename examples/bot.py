c82736bd09357633bf0fa0db7be4d7fe6f67b7f6
DaPandaOfficial <57327793+MiroslavRosenov@users.noreply.github.com>
2022-12-01 10:38:15 +0200

    Update bot.py

--- examples/bot.py
+++ examples/bot.py
@@ -1,5 +1,8 @@
 import discord
+from typing import Dict
 from discord.ext import commands, ipc
+from discord.ext.ipc.server import Server
+from discord.ext.ipc.objects import ClientPayload
 
 class MyBot(commands.Bot):
     def __init__(self) -> None:
@@ -13,4 +16,9 @@ class MyBot(commands.Bot):
         self.ipc = ipc.Server(self, secret_key="🐼")
 
     async def setup_hook(self) -> None:
-        await self.load_extension("cog")
+        await self.ipc.start()
+
+    @Server.route()
+    async def get_user_data(self, data: ClientPayload) -> Dict:
+        user = self.get_user(data.user_id)
+        return user._to_minimal_user_json()
